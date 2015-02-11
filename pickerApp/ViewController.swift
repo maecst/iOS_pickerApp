4bf00d5ebbe3b94d9b4ae643e31a6b292c14568b
Mae Yee <Mae@Housam-Halliss-MacBook-Pro.local>
2015-02-10 18:12:00 -0800

    - The CODE Lady Tutorial on YouTube (https://youtube.com/user/thecodelady)

--- pickerApp/ViewController.swift
+++ pickerApp/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate{
+    
+    var food = ["apples", "bananas", "grapes", "oranges", "pears", "strawberries"]
+    var provinces = ["Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland", "Northwest Territories", "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan", "Yukon"]
+    
+    @IBOutlet weak var itemLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        itemLabel.text = provinces[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // mandatory methods required for pickerView
+    
+    // returns the number of 'columns' to display.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // returns the # of rows in each component..
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return provinces.count
+    }
+    
+    // these methods return either a plain NSString, a NSAttributedString, or a view (e.g UILabel) to display the row for the component.
+    // for the view versions, we cache any hidden and thus unused views and pass them back for reuse.
+    // If you return back a different object, the old one will be released. the view will be centered in the row rect
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        
+        return provinces[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        var itemSelected = provinces[row]
+        itemLabel.text = itemSelected
+        
+    }
 
 }
 
